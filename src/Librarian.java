fd329c9e3a12dad3aa5c55db8d574e7b52969d12
aj-cronin <aj.cronin14@gmail.com>
2024-09-30 13:35:45 -0700

    Added Initial Methods

--- src/Librarian.java
+++ src/Librarian.java
@@ -1,3 +1,39 @@
 public class Librarian {
     
+    public Librarian() {
+    }
+
+    public String search(String method) {
+        // if (method.equals("title")) {
+        //     return searchByTitle;
+        // } else if (method.equals("author"){
+        //     return searchByAuthor;
+        // } else if (method.equals("rating"){
+        //     return searchByRating;
+        // }
+        return "temp";
+    }
+    public void addBook(Book newBook) {
+
+    }
+
+    public void setToRead(Book bookToUpdate) {
+
+    }
+
+    public void rate(Book bookToUpdate) {
+
+    }
+
+    public void getBooks(String method) {
+
+    }
+
+    public Book suggestRead() {
+        return new Book("temp", "temp");
+    }
+
+    public void addBooks(String fileName) {
+
+    }
 }
