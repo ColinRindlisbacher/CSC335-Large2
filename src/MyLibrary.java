4ebe9f7b67217e8684fde2d519ab8bbc6287b98d
ColinRindlisbacher <colinrindlisbacher@gmail.com>
2024-10-01 16:15:25 -0700

    removed unused Library object in MyLibrary

--- src/MyLibrary.java
+++ src/MyLibrary.java
@@ -12,7 +12,6 @@ public class MyLibrary {
 
 	public static void main(String[] args) {
 		LibraryController libCon = new LibraryController();
-		Librarian lib = new Librarian();
 		Scanner s = new Scanner(System.in);
 		String input = "";
 
