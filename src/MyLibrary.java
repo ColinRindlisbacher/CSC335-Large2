5308577b0dd619c5aab169718e11f551da9e26ec
ColinRindlisbacher <colinrindlisbacher@gmail.com>
2024-09-30 17:33:38 -0700

    Finished user input loop, not fully tested tho

--- src/MyLibrary.java
+++ src/MyLibrary.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class MyLibrary {
@@ -80,21 +82,87 @@ public class MyLibrary {
 					title = s.nextLine();
 					System.out.println("Enter author for book you want to rate:");
 					author = s.nextLine();
+					int rating = 0;
 					boolean isValidRate = false;
+			
 					// loop to get valid rating(1-5)
+					while(!isValidRate){
+						System.out.println("Please enter rating for book(1-5):");
+						rating = s.nextInt();
+
+						isValidRate = rating >= 1 && rating <= 5;
+						if(!isValidRate){
+							System.out.println("Invalid input. Please enter an integer 1-5");
+						}
+					}
+					lib.rate(title, author, rating);
 					break;
 
+				// getBooks selected
 				case "5":
+					isValidMethod = false;
+					method = "";
+					
+					while(!isValidMethod){
+						System.out.println("Please select what method you would like to sort the books(title,author,rating,unread)");
+						method = s.nextLine();
+						isValidMethod = method.equalsIgnoreCase("title")  ||
+										method.equalsIgnoreCase("author") ||
+										method.equalsIgnoreCase("rating") ||
+										method.equalsIgnoreCase("unread");
+
+						// print error message if bad input
+						if(!isValidMethod){
+							System.out.println("Invalid input. Please enter 'title', 'author', 'rating', or 'unread'.");
+						}
+					}
+					lib.getBooks(method);
 					break;
 
+				// suggestRead selected
 				case "6":
+					Book suggestion = lib.suggestRead();
+					System.out.println("Here is a book suggestion that you haven't read:");
+					System.out.println(suggestion.toString());
 					break;
-					
+
+				// addBooks selected
 				case "7":
+					System.out.println("Please enter file name:");
+					String fileName = s.nextLine();
+
+				    try {
+						File file = new File(fileName);
+						Scanner fiScanner = new Scanner(file);
+						
+						while(fiScanner.hasNextLine()){
+							String line = fiScanner.nextLine();
+							String[] bookInfo = line.split(";");
+							// make sure line is valid length of 2
+							if(bookInfo.length == 2){
+								String newTitle = bookInfo[0].trim();
+								String newAuthor = bookInfo[1].trim();
+								Book fileBook = new Book(newTitle, newAuthor);
+								lib.addBook(fileBook);
+							}
+						}
+						fiScanner.close();
+					} catch (FileNotFoundException e) {
+						System.out.println("File not found: " + fileName);
+					}
+					break;
+
+				// if no other valid option is selected
+				default:
+					// makes sure quit not selected
+					if(!input.equalsIgnoreCase("q")){
+						System.out.println("Error input is not a valid option, try again.");
+					}
 					break;
 			}
 		}
 		s.close();
+		System.out.println("Thank you for vising CroRind Library, have a good day!");
 	}
 
 }
